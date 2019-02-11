6661fbf1069dca4c1cca823f9f51fb3f66b376c9
fperez371 <francisco.e.perez@gmail.com>
2019-02-10 22:59:52 -0500

    some game logic, styling for page

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ document.addEventListener("DOMContentLoaded", ()=>{
   console.log("webpack is running...");
   
   var canvas = document.createElement('canvas'); 
-  canvas.width = 600;
-  canvas.height = 600;
+  canvas.width = 512;
+  canvas.height = 512;
   var ctx = canvas.getContext('2d'); 
   // var background = new Image();
-  // background.src = "./images/arena.png";
+  // background.src = "../images/arena.png";
   // background.addEventListener("load", loadImage, false);
   document.getElementById('main-content').appendChild(canvas);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
 });
 
-// function loadImage(e){
-//   ctx.drawImage(background, 0, 0);
-// }
+
