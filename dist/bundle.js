34333ff94a7f75d4bf2c8e5da9cbdf32b09cb6ed
fperez371 <francisco.e.perez@gmail.com>
2019-03-18 11:25:36 -0400

    changed font to dbz themed font

--- dist/bundle.js
+++ dist/bundle.js
@@ -139,32 +139,38 @@ function () {
     this.width = 33;
     this.height = 40;
     this.img = new Image();
-    this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png'; // img.addEventListener("load", loadImage, false);
+    this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku.png"; // img.addEventListener("load", loadImage, false);
 
     this.animate = this.animate.bind(this);
-    this.canvas = document.getElementById('canvas');
-    this.ctx = this.canvas.getContext('2d');
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
     this.shift = [0, -1];
     this.totalFrames = 8;
     this.currentFrame = 1;
     this.pos = [200, 450];
     this.check = 0;
-    this.dir = 'idle';
+    this.dir = "idle";
     this.handlekeydown = this.handlekeydown.bind(this);
     this.GOKUDIRS = {
       idle: [0, -1],
       right: [-1, 83],
-      left: [1151, 83]
+      left: [1151, 83],
+      punching: [-4, 479]
     };
     this.HEIGHTS = {
       idle: 40,
       running: 48,
       punching: 40
     };
+    this.WIDTHS = {
+      idle: 33,
+      running: 33,
+      punching: 40
+    };
     this.TOTALFRAMES = {
       idle: 8,
       running: 8,
-      punching: 15
+      punching: 7
     }; // this.kick = this.kick.bind(this);
     // this.punch = this.punch.bind(this);
   }
@@ -172,41 +178,36 @@ function () {
   _createClass(Goku, [{
     key: "handleDir",
     value: function handleDir() {
-      if (this.dir === 'right' && (this.shift[1] !== this.GOKUDIRS.right[1] || this.shift[0] > 230)) {
-        this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
+      if (this.dir === "right" && (this.shift[1] !== this.GOKUDIRS.right[1] || this.shift[0] > 230)) {
+        this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku.png";
         this.pos[1] = 444;
         this.shift = this.GOKUDIRS.right.slice();
         this.height = this.HEIGHTS.running;
+        this.width = this.WIDTHS.running;
         this.currentFrame = 1;
         this.totalFrames = this.TOTALFRAMES.running;
       } else if (this.dir === "left" && (this.shift[1] !== this.GOKUDIRS.left[1] || this.shift[0] < 500)) {
-        this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku_left.png';
+        this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku_left.png";
         this.pos[1] = 444;
         this.shift = this.GOKUDIRS.left.slice();
         this.height = this.HEIGHTS.running;
+        this.width = this.WIDTHS.running;
         this.currentFrame = 1;
         this.totalFrames = this.TOTALFRAMES.running;
-      } else if (this.dir === 'idle' && this.shift[1] !== this.GOKUDIRS.idle[1]) {
-        this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
+      } else if (this.dir === "idle" && this.shift[1] !== this.GOKUDIRS.idle[1]) {
+        this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku.png";
         this.pos[1] = 450;
         this.shift = this.GOKUDIRS.idle.slice();
         this.height = this.HEIGHTS.idle;
+        this.width = this.WIDTHS.idle;
         this.currentFrame = 1;
         this.totalFrames = this.TOTALFRAMES.idle;
-      } else if (this.dir === 'up' && this.shift[1] !== this.GOKUDIRS.idle[1]) {
-        this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
-        this.pos[1] = 450;
-        this.shift = this.GOKUDIRS.idle.slice();
-        this.height = this.HEIGHTS.idle;
+      } else if (this.dir === "punching") {
+        this.shift = this.GOKUDIRS.punching.slice();
+        this.width = this.WIDTHS.punching;
+        this.heigth = this.HEIGHTS.punching;
         this.currentFrame = 1;
-        this.totalFrames = this.TOTALFRAMES.idle;
-      } else if (this.dir === 'idle' && this.shift[1] !== this.GOKUDIRS.idle[1]) {
-        this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
-        this.pos[1] = 450;
-        this.shift = this.GOKUDIRS.idle.slice();
-        this.height = this.HEIGHTS.idle;
-        this.currentFrame = 1;
-        this.totalFrames = this.TOTALFRAMES.idle;
+        this.totalFrames = this.TOTALFRAMES.punching;
       }
     }
   }, {
@@ -214,26 +215,20 @@ function () {
     value: function handlekeydown(e) {
       e.preventDefault();
 
-      if (e.key === 'a') {
-        this.dir = 'left';
+      if (e.key === "a") {
+        this.dir = "left";
         this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
         this.handleDir();
       }
 
-      if (e.key === 'w') {
-        this.dir = "up";
+      if (e.key === "d") {
+        this.dir = "right";
         this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
         this.handleDir();
       }
 
-      if (e.key === 'd') {
-        this.dir = 'right';
-        this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
-        this.handleDir();
-      }
-
-      if (e.key === 's') {
-        this.dir = "down";
+      if (e.key === "j") {
+        this.dir = "punching";
         this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
         this.handleDir();
       }
@@ -267,9 +262,9 @@ function () {
   }, {
     key: "inBounds",
     value: function inBounds() {
-      if (this.pos[0] > 480 && this.dir === 'right') {
+      if (this.pos[0] > 480 && this.dir === "right") {
         return false;
-      } else if (this.pos[0] < 0 && this.dir === 'left') {
+      } else if (this.pos[0] < 0 && this.dir === "left") {
         return false;
       }
 
@@ -280,10 +275,8 @@ function () {
     value: function move(dir) {
       switch (dir) {
         case "right":
-          // debugger
           if (this.inBounds()) {
             this.pos[0] += 1;
-            debugger;
           }
 
           break;
@@ -291,6 +284,20 @@ function () {
         case "left":
           if (this.inBounds()) {
             this.pos[0] -= 1;
+          }
+
+          break;
+
+        case "up":
+          if (this.inBounds()) {
+            this.pos[1] -= 1;
+          }
+
+          break;
+
+        case "down":
+          if (this.inBounds()) {
+            this.pos[1] += 1;
           }
 
           break;
@@ -313,9 +320,11 @@ function () {
       } else {
         this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
 
-        if (this.dir === 'left') {
+        if (this.dir === "left") {
           this.shift[0] -= this.width;
-        } else {
+        } else if (this.dir === "right") {
+          this.shift[0] += this.width;
+        } else if (this.dir === "punching") {
           this.shift[0] += this.width;
         }
 
--- src/goku.js
+++ src/goku.js
@@ -14,11 +14,6 @@
 //     this.pos = [470, 430];
 //   }
 
-
-
-
-
-
 //   animate() {
 //     this.ctx.clearRect(32, 40, 512, 512);
 //     this.ctx.drawImage(this.img, this.shift, 0, this.width, this.height,
@@ -38,110 +33,103 @@ export default class Goku {
     this.width = 33;
     this.height = 40;
     this.img = new Image();
-    this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
+    this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku.png";
     // img.addEventListener("load", loadImage, false);
     this.animate = this.animate.bind(this);
-    this.canvas = document.getElementById('canvas');
-    this.ctx = this.canvas.getContext('2d');
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
     this.shift = [0, -1];
     this.totalFrames = 8;
     this.currentFrame = 1;
     this.pos = [200, 450];
     this.check = 0;
-    this.dir = 'idle';
-    this.handlekeydown = this.handlekeydown.bind(this)
-
-
+    this.dir = "idle";
+    this.handlekeydown = this.handlekeydown.bind(this);
 
     this.GOKUDIRS = {
       idle: [0, -1],
       right: [-1, 83],
       left: [1151, 83],
+      punching: [-4, 479],
     };
 
     this.HEIGHTS = {
       idle: 40,
       running: 48,
-      punching: 40
+      punching: 40,
     };
 
+    this.WIDTHS = {
+      idle: 33,
+      running: 33,
+      punching: 40,
+    };
 
     this.TOTALFRAMES = {
       idle: 8,
       running: 8,
-      punching: 15,
-
+      punching: 7,
     };
 
     // this.kick = this.kick.bind(this);
     // this.punch = this.punch.bind(this);
   }
 
-
-
   handleDir() {
-    if (this.dir === 'right' && (this.shift[1] !== this.GOKUDIRS.right[1] || this.shift[0] > 230)) {
-      this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
+    if (
+      this.dir === "right" &&
+      (this.shift[1] !== this.GOKUDIRS.right[1] || this.shift[0] > 230)
+    ) {
+      this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku.png";
       this.pos[1] = 444;
       this.shift = this.GOKUDIRS.right.slice();
       this.height = this.HEIGHTS.running;
+      this.width = this.WIDTHS.running;
       this.currentFrame = 1;
       this.totalFrames = this.TOTALFRAMES.running;
-    } else if (this.dir === "left" && (this.shift[1] !== this.GOKUDIRS.left[1] || this.shift[0] < 500)) {
-      this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku_left.png';
+    } else if (
+      this.dir === "left" &&
+      (this.shift[1] !== this.GOKUDIRS.left[1] || this.shift[0] < 500)
+    ) {
+      this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku_left.png";
       this.pos[1] = 444;
       this.shift = this.GOKUDIRS.left.slice();
       this.height = this.HEIGHTS.running;
+      this.width = this.WIDTHS.running;
       this.currentFrame = 1;
       this.totalFrames = this.TOTALFRAMES.running;
-    } else if (this.dir === 'idle' && this.shift[1] !== this.GOKUDIRS.idle[1]) {
-      this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
+    } else if (this.dir === "idle" && this.shift[1] !== this.GOKUDIRS.idle[1]) {
+      this.img.src = "/Users/fp/Desktop/JSBeatDown/images/goku.png";
       this.pos[1] = 450;
       this.shift = this.GOKUDIRS.idle.slice();
       this.height = this.HEIGHTS.idle;
+      this.width = this.WIDTHS.idle;
       this.currentFrame = 1;
       this.totalFrames = this.TOTALFRAMES.idle;
-    }
-    else if (this.dir === 'up' && this.shift[1] !== this.GOKUDIRS.idle[1]) {
-      this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
-      this.pos[1] = 450;
-      this.shift = this.GOKUDIRS.idle.slice();
-      this.height = this.HEIGHTS.idle;
+    } else if (this.dir === "punching") {
+      this.shift = this.GOKUDIRS.punching.slice();
+      this.width = this.WIDTHS.punching;
+      this.heigth = this.HEIGHTS.punching;
       this.currentFrame = 1;
-      this.totalFrames = this.TOTALFRAMES.idle;
-    } else if (this.dir === 'idle' && this.shift[1] !== this.GOKUDIRS.idle[1]) {
-      this.img.src = '/Users/fp/Desktop/JSBeatDown/images/goku.png';
-      this.pos[1] = 450;
-      this.shift = this.GOKUDIRS.idle.slice();
-      this.height = this.HEIGHTS.idle;
-      this.currentFrame = 1;
-      this.totalFrames = this.TOTALFRAMES.idle;
+      this.totalFrames = this.TOTALFRAMES.punching;
     }
   }
-
 
   handlekeydown(e) {
     e.preventDefault();
 
-    if (e.key === 'a') {
-      this.dir = 'left';
-
-
+    if (e.key === "a") {
+      this.dir = "left";
       this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
       this.handleDir();
     }
-    if (e.key === 'w') {
-      this.dir = "up";
+    if (e.key === "d") {
+      this.dir = "right";
       this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
       this.handleDir();
     }
-    if (e.key === 'd') {
-      this.dir = 'right';
-      this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
-      this.handleDir();
-    }
-    if (e.key === 's') {
-      this.dir = "down";
+    if (e.key === "j") {
+      this.dir = "punching";
       this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
       this.handleDir();
     }
@@ -152,12 +140,10 @@ export default class Goku {
     this.handleDir();
   }
 
-  runRight() {
-
-  }
+  runRight() {}
 
   start() {
-    document.addEventListener("keydown", (key) => this.handlekeydown(key));
+    document.addEventListener("keydown", key => this.handlekeydown(key));
     document.addEventListener("keyup", () => this.handlekeyup());
     this.animate();
   }
@@ -170,27 +156,34 @@ export default class Goku {
 
   // }
   inBounds() {
-    if (this.pos[0] > 480 && this.dir === 'right') {
+    if (this.pos[0] > 480 && this.dir === "right") {
       return false;
-    } else if (this.pos[0] < 0 && this.dir === 'left') {
+    } else if (this.pos[0] < 0 && this.dir === "left") {
       return false;
     }
     return true;
   }
 
   move(dir) {
-
     switch (dir) {
-      case ("right"):
-        // debugger
+      case "right":
         if (this.inBounds()) {
           this.pos[0] += 1;
-          debugger
         }
         break;
-      case ("left"):
+      case "left":
         if (this.inBounds()) {
           this.pos[0] -= 1;
+        }
+        break;
+      case "up":
+        if (this.inBounds()) {
+          this.pos[1] -= 1;
+        }
+        break;
+      case "down":
+        if (this.inBounds()) {
+          this.pos[1] += 1;
         }
         break;
       default:
@@ -198,30 +191,45 @@ export default class Goku {
     }
   }
 
-
-
   animate() {
     if (this.check < 7) {
       this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
-      this.ctx.drawImage(this.img, this.shift[0], this.shift[1], this.width, this.height,
-        this.pos[0], this.pos[1], this.width, this.height);
+      this.ctx.drawImage(
+        this.img,
+        this.shift[0],
+        this.shift[1],
+        this.width,
+        this.height,
+        this.pos[0],
+        this.pos[1],
+        this.width,
+        this.height
+      );
 
       if (this.currentFrame === this.totalFrames) {
         this.shift = this.GOKUDIRS[this.dir].slice();
         this.currentFrame = 1;
       }
-
     } else {
       this.ctx.clearRect(this.pos[0], this.pos[1], 512, 512);
-      if (this.dir === 'left') {
+      if (this.dir === "left") {
         this.shift[0] -= this.width;
-      } else {
+      } else if (this.dir === "right") {
+        this.shift[0] += this.width;
+      } else if (this.dir === "punching") {
         this.shift[0] += this.width;
       }
-      this.ctx.drawImage(this.img, this.shift[0], this.shift[1], this.width, this.height,
-        this.pos[0], this.pos[1], this.width, this.height);
-
-
+      this.ctx.drawImage(
+        this.img,
+        this.shift[0],
+        this.shift[1],
+        this.width,
+        this.height,
+        this.pos[0],
+        this.pos[1],
+        this.width,
+        this.height
+      );
 
       this.currentFrame++;
       this.check = 0;
